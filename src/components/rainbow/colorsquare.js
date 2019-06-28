2e392e3e7a6dc267d73a79b37b6782fef9b58177
nebevets <steveben@exmsft.com>
2019-06-27 18:55:08 -0700

    made colors change on interval

--- src/components/rainbow/colorsquare.js
+++ src/components/rainbow/colorsquare.js
@@ -5,9 +5,20 @@ import {randomColor} from './randcolor';
 class ColorSquare extends Component{
   constructor(props){
     super(props);
+    this.interval = null;
     this.state = {
       color: randomColor()
     }
+  }
+  componentDidMount(){
+    if(!this.interval){
+      this.interval = setInterval(this.handleMouseEnter.bind(this), Math.floor(Math.random() * (10000-1000+1))+1000);
+      return;
+    }
+    clearInterval(this.interval);
+  }
+  componentWillUnmount(){
+    clearInterval(this.interval);
   }
   handleMouseEnter(){
     const newColor = randomColor();
@@ -18,7 +29,7 @@ class ColorSquare extends Component{
   render(){
     const {color} = this.state;
     return (
-      <div className="square" style={{'backgroundColor': color}} onMouseEnter={this.handleMouseEnter.bind(this)}></div>
+      <div className="square" style={{'backgroundColor': color}}></div>
     );  
   }
 }
